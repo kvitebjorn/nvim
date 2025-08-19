84a666fcf97da608bbd0f9f9084f26e9de49283b
Kyle Harrington <harintonkairu@gmail.com>
2025-08-18 22:06:15 -0600

    fix title thing

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- ~/.config/nvim/lua/config/options.lua
+
+-- Set up my title for waybar
+vim.o.title = true
+vim.o.titlestring = "nvim: %t"
+
+-- Use system clipboard for all yank/delete/put
+vim.opt.clipboard = "unnamedplus"
--- lua/plugins/title.lua
+++
@@ -1,5 +0,0 @@
--- Tell Neovim to set the terminal/window title
-vim.o.title = true
-
--- Format: "nvim: filename"
-vim.o.titlestring = "nvim: %t"
