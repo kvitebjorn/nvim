c6acf7c44198daab61ce47d5e733ce5a34d0e02f
Kyle Harrington <harintonkairu@gmail.com>
2025-08-23 10:40:19 -0600

    add lsps

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,36 @@
+return {
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    opts = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              workspace = { checkThirdParty = false },
+              telemetry = { enable = false },
+            },
+          },
+        },
+        asm_lsp = {},
+        clangd = {},
+        gopls = {},
+      },
+    },
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "asm-lsp",
+        "clangd",
+        "gopls",
+      },
+    },
+  },
+}
